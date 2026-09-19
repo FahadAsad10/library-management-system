118ac4a053fa240084a240c24d9df0a30684a47f
Fahad Asad <m.fahadasadshaikh@gmail.com>
2026-09-20 00:44:43 +0500

    Add Book class with basic fields and toString

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Book book1 = new Book("111", "Clean Code", "Robert Martin");
+        Book book2 = new Book("222", "The Pragmatic Programmer", "Andy Hunt");
+
+        System.out.println(book1);
+        System.out.println(book2);
+    }
+}
