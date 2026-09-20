699af68b47ec86ac34de3be216f0dc46a5051716
Fahad Asad <m.fahadasadshaikh@gmail.com>
2026-09-20 15:17:21 +0500

    Add Member class that can borrow and return books

--- Main.java
+++ Main.java
@@ -1,9 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         Book book1 = new Book("111", "Clean Code", "Robert Martin");
-        Book book2 = new Book("222", "The Pragmatic Programmer", "Andy Hunt");
+        Member fahad = new Member("Fahad", "M001");
 
-        System.out.println(book1);
-        System.out.println(book2);
+        fahad.borrowBook(book1);
+        book1.setBorrowed(true);
+
+        System.out.println(fahad.getName() + " is holding:");
+        for (Book b : fahad.getBorrowedBooks()) {
+            System.out.println(" - " + b);
+        }
     }
 }
---
+++ Member.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Member {
+    private String name;
+    private String memberId;
+    private List<Book> borrowedBooks = new ArrayList<>();
+
+    public Member(String name, String memberId) {
+        this.name = name;
+        this.memberId = memberId;
+    }
+
+    public String getName() { return name; }
+    public List<Book> getBorrowedBooks() { return borrowedBooks; }
+
+    public void borrowBook(Book book) {
+        borrowedBooks.add(book);
+    }
+
+    public void returnBook(Book book) {
+        borrowedBooks.remove(book);
+    }
+}
